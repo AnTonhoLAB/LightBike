82b57f9bb2aee5bf9411fd7a364fb93ac86a55f6
George Gomes <georgegomees@gmail.com>
2018-12-11 16:27:28 -0200

    Start project

---
+++ LightBike/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LightBike
+//
+//  Created by George Gomes on 11/12/18.
+//  Copyright © 2018 George Gomes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LightBike/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  LightBike
+//
+//  Created by George Gomes on 11/12/18.
+//  Copyright © 2018 George Gomes. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
