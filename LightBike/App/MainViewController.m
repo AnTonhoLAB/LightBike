81e973971c4244449e9ffa4339678408bb4a557c
George Gomes <georgegomees@gmail.com>
2018-12-11 16:35:11 -0200

    Update file names

--- LightBike/App/MainViewController.m
+++ LightBike/App/MainViewController.m
@@ -6,13 +6,13 @@
 //  Copyright © 2018 George Gomes. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "MainViewController.h"
 
-@interface ViewController ()
+@interface MainViewController ()
 
 @end
 
-@implementation ViewController
+@implementation MainViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
